38262f74ce47810e666f2e04ad1ee3568db93cf4
ShayFae <ifrahnoor108@gmail.com>
2022-03-18 01:56:30 -0400

    Add register query to users route

--- server/app.js
+++ server/app.js
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 const db = require('./configs/db.config');
 
 // routes
-app.use('/api/users', usersRoutes(db));
-app.use('/api/games', gamesRoutes(db));
+app.use('/users', usersRoutes(db));
+app.use('/games', gamesRoutes(db));
 
 app.get('/', (req, res) => {
 	res.json({ greetings: 'hello world' });
