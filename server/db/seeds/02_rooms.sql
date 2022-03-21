662f8f31d8229a6dca2d381a19ba1517ab3985ff
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-03-21 16:21:10 -0400

    add more data in users/rooms/appointments/games table

--- server/db/seeds/02_rooms.sql
+++ server/db/seeds/02_rooms.sql
@@ -2,4 +2,6 @@ INSERT INTO rooms (id) VALUES (1);
 INSERT INTO rooms (id) VALUES (2);
 INSERT INTO rooms (id) VALUES (3);
 INSERT INTO rooms (id) VALUES (4);
+INSERT INTO rooms (id) VALUES (5);
+
 
--- server/db/seeds/05_games.sql
+++ server/db/seeds/05_games.sql
@@ -2,4 +2,4 @@ INSERT INTO games (name, image, category_id) VALUES ('Lost Ark', 'https://images
 INSERT INTO games (name, image, category_id) VALUES ('Valorant', 'https://stanforddaily.com/wp-content/uploads/2020/08/image-2.png', 1);
 INSERT INTO games (name, image, category_id) VALUES ('Risk of Rain 2', 'https://upload.wikimedia.org/wikipedia/en/c/c1/Risk_of_Rain_2.jpg', 1);
 INSERT INTO games (name, image, category_id) VALUES ('UNO', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/UNO_Logo.svg/1200px-UNO_Logo.svg.png', 2);
-INSERT INTO games (name, image, category_id) VALUES ('CATAN', 'https://upload.wikimedia.org/wikipedia/en/a/a3/Catan-2015-boxart.jpg', 2);
+INSERT INTO games (name, image, category_id) VALUES ('CATAN', 'https://upload.wikimedia.org/wikipedia/en/a/a3/Catan-2015-boxart.jpg', 3);
