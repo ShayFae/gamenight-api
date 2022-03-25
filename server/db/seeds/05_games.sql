22aa70e44f0b7894f24563e19f0271d996cfd61f
ShayFae <ifrahnoor108@gmail.com>
2022-03-25 17:35:41 -0400

    Add more seeds

--- server/db/seeds/05_games.sql
+++ server/db/seeds/05_games.sql
@@ -3,3 +3,10 @@ INSERT INTO games (name, image, category_id) VALUES ('Valorant', 'https://stanfo
 INSERT INTO games (name, image, category_id) VALUES ('Risk of Rain 2', 'https://upload.wikimedia.org/wikipedia/en/c/c1/Risk_of_Rain_2.jpg', 1);
 INSERT INTO games (name, image, category_id) VALUES ('UNO', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/UNO_Logo.svg/1200px-UNO_Logo.svg.png', 2);
 INSERT INTO games (name, image, category_id) VALUES ('CATAN', 'https://upload.wikimedia.org/wikipedia/en/a/a3/Catan-2015-boxart.jpg', 3);
+INSERT INTO games (name, image, category_id) VALUES ('League of Legends', 'https://staticg.sportskeeda.com/editor/2021/01/dffdc-16120717956166-800.jpg', 1);
+INSERT INTO games (name, image, category_id) VALUES ('Counter-Strike: Global Offensive', 'https://cdn.mos.cms.futurecdn.net/ZPwx6tmYKQtnPZDRCAfXMj.jpg', 1);
+INSERT INTO games (name, image, category_id) VALUES ('Destiny 2', 'https://destiny.wiki.gallery/images/thumb/0/0e/Vow_of_the_Disciple.jpg/1600px-Vow_of_the_Disciple.jpg', 1);
+INSERT INTO games (name, image, category_id) VALUES ('Cards Against Humanity', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Cards_Against_Humanity_logo_2009.svg/800px-Cards_Against_Humanity_logo_2009.svg.png', 2);
+INSERT INTO games (name, image, category_id) VALUES ('Magic: the Gathering',  'https://imageio.forbes.com/blogs-images/laurenorsini/files/2019/06/EN_PauperFormat_Header-1200x700.jpg?format=jpg&width=1200&fit=bounds', 2);
+INSERT INTO games (name, image, category_id) VALUES ('Civilization VI', 'https://cdn.vox-cdn.com/thumbor/ezO6Tuib470ku37q9HPSnopKAwE=/0x0:1158x651/1200x800/filters:focal(487x234:671x418)/cdn.vox-cdn.com/uploads/chorus_image/image/66827138/civ6.0.0.png', 1);
+INSERT INTO games (name, image, category_id) VALUES ('Jack Box Party Pack', 'https://cdn.akamai.steamstatic.com/steam/apps/434170/capsule_616x353.jpg?t=1597699274', 1);
